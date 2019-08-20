fe870c3e2fcb08fd3be91d8f2c2a72d6aea45d67
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2019-08-20 15:18:21 +0200

    Handle permissions

--- commands/off.js
+++ commands/off.js
@@ -1,7 +1,48 @@
 Command = require('./command.js');
 
 Command.Off = {
-    log: function() {
-        console.log("yo");
+    name: "off",
+    alias: [
+        "off",
+        "shutdown",
+        "disconnect"
+    ],
+    groupOption: {
+        whitelist: ["Roi"],
+        blacklist: []
+    },
+    channelOption: {
+        whitelist: [],
+        blacklist: []
+    },
+    description: "turn the bot off",
+
+
+    match: function(command) {
+        return this.alias.includes(command);
+    },
+    checkPerm: function(channel, role) {
+        // check blacklists
+        if(this.groupOption.blacklist.length > 0 && this.groupOption.blacklist.includes(role.name)) {
+            return false;
+        }
+        if(this.channelOption.blacklist.length > 0 && this.channelOption.blacklist.includes(channel.id)) {
+            return false;
+        }
+    
+        // check whitelists
+        if(this.groupOption.whitelist.length > 0 && !this.groupOption.whitelist.includes(role.name)) {
+            return false;
+        }
+        if(this.channelOption.whitelist.length > 0 && !this.channelOption.whitelist.includes(channel.id)) {
+            return false;
+        }
+    
+        return true;
+    },
+
+    shutdown: function(Phoenix) {
+        console.log('Phoenix disconnected.')
+        Phoenix.bot.destroy();
     }
 }
--- index.js
+++ index.js
@@ -26,6 +26,35 @@ bot.on('ready', () => {
     Phoenix.testChannel = Phoenix.guild.channels.find(chan => chan.id == Config.testChannel);
 
     Phoenix.testChannel.send("Phoenix connecté");
+});
 
-    // Command.Off.log(Command.Off.login);
-})
+bot.on('message', (msg) => {
+    if (msg.content.startsWith(Config.prefix)) {
+        console.log(msg.author.username + ' : ' + msg.content);
+        let msgParts = msg.content.split(' ');
+        ReadCommand(msgParts[0].slice(1), msgParts.slice(1), msg);
+    }
+});
+
+function ReadCommand(command, args, msg) {
+    if(Config.everyoneBlackListed && GetGuildMember(msg.author).roles.length == 0) {
+        return;
+    }
+    if (Command.Off.match(command)) {
+        if(!Command.Off.checkPerm(command, GetGuildMember(msg.author).highestRole)) {
+            PermissionDenied(msg);
+            return;
+        }
+        msg.delete()
+        .then(Command.Off.shutdown(Phoenix));
+    }
+}
+
+function PermissionDenied(msg) {
+    console.log('Permission denied');
+    msg.reply("Patouche");
+}
+
+function GetGuildMember(user) {
+    return Phoenix.guild.members.find(member => member.id == user.id);
+}
