807c14802b70685598acf6b88d19b421ec5f787e
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2020-02-09 11:05:04 +0100

    Set help message to rich embed

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,7 @@
 let Command = require('../src/Command');
 let commands = require('./command');
 let Play = require('./play');
+const {RichEmbed} = require('discord.js');
 
 module.exports = class Help extends Command {
     constructor(author) {
@@ -15,21 +16,24 @@ module.exports = class Help extends Command {
     static description = "Affiche la liste des commandes";
 
     static call(msg, Phoenix) {
-        let text = "Commandes : \n\n"+
-        "help: Affiche la liste des commandes\n"+
-        "off: turn the bot off\n"+
-        "clear: undefined\n"+
-        "play [nom/url]: Ajoute une musique à la file d'attente. Démarre la lecture si aucune musique n'est en cours.\n"+
-        "skip: Passer à la prochaine musique de la file d'attente\n"+
-        "stop: Arrete la musique et deconnecte le bot du salon vocal.\n"+
-        "volume [0-200]: Changer le volume\n"+
-        "playlist: Gérer les playlist. !playlist help\n"+
-        "queue: Affiche la liste d'attente des musiques.\n"+
-        "shop: Intéragir avec le marché de Phoenix";
-        
-        msg.channel.send(text, {
-            code: true
-        })
-        .catch(console.error);
+        let embed = new RichEmbed();
+        embed.setTitle("Listes des commandes: ")
+            .setColor('ORANGE')
+            .setThumbnail(Phoenix.bot.user.avatarURL)
+            .setFooter('Codé par GabRay', msg.author.avatarURL)
+            .addField(Phoenix.config.prefix + 'help', 'Affiche la liste des commandes.')
+            .addField(Phoenix.config.prefix + 'off', 'Redémarre le bot.')
+            .addField(Phoenix.config.prefix + 'clear', 'Efface les messages de commande bot.')
+            .addField(Phoenix.config.prefix + 'play [nom/url]', "Ajoute une musique à la file d'attente. Démarre la lecture si aucune musique n'est en cours.")
+            .addField(Phoenix.config.prefix + 'skip', "Passer à la prochaine musique de la fille d'attente.")
+            .addField(Phoenix.config.prefix + 'stop', "Arrete la musique.")
+            .addField(Phoenix.config.prefix + 'pause', "Met la musique en pause.")
+            .addField(Phoenix.config.prefix + 'resume', "Reprend la lecture de la musique.")
+            .addField(Phoenix.config.prefix + 'volume [0-200]', "Règler le volume.")
+            .addField(Phoenix.config.prefix + 'queue', "Affiche la liste d'attente.")
+            .addField(Phoenix.config.prefix + 'playlist', "Gérer les playlists. `"+Phoenix.config.prefix+"playlist help`.")
+            .addField(Phoenix.config.prefix + 'shop', "Intéragir avec le marché de Phoenix.")
+            .addBlankField()
+        msg.channel.send(embed);
     }
 }
